a17bbff51790bfa1e8c28082b64b606cbebfd0a3
Backa <phatkonlol@gmail.com>
2023-12-20 06:09:16 +0600

    permission

--- books_app/store/tests/test_serializers.py
+++ books_app/store/tests/test_serializers.py
@@ -1,28 +1,43 @@
 from unittest import TestCase
 
+from django.contrib.auth.models import User
+
 from store.models import Book
 from store.serializers import BookSerializer
 
 
 class BookSerializerTestCase(TestCase):
+    def setUp(self):
+        self.user_1 = User.objects.create(username="user1", password="password")
+        self.user_2 = User.objects.create(username="user2", password="password")
+
     def test_ok(self):
         book_1 = Book.objects.create(name="Test book 1", price=25,
-                                     author_name="Author 1")
+                                     author_name="Author 1",
+                                     owner=self.user_1)
         book_2 = Book.objects.create(name="Test book 2", price=55,
-                                     author_name="Author 2")
+                                     author_name="Author 2",
+                                     owner=self.user_2)
         data = BookSerializer([book_1, book_2], many=True).data
         expected_data = [
             {
                 'id': book_1.id,
                 'name': 'Test book 1',
                 'price': '25.00',
-                'author_name': 'Author 1'
+                'author_name': 'Author 1',
+                'owner': book_1.owner.id
             },
             {
                 'id': book_2.id,
                 'name': 'Test book 2',
                 'price': '55.00',
-                'author_name': 'Author 2'
+                'author_name': 'Author 2',
+                'owner': book_2.owner.id
             },
         ]
+        # print("Expected data:", expected_data)
+        # print("Actual data:", data)
+        # print("User 1 ID:", self.user_1.id)
+        # print("User 2 ID:", self.user_2.id)
+
         self.assertEqual(expected_data, data)
--- books_app/store/views.py
+++ books_app/store/views.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnl
 from rest_framework.viewsets import ModelViewSet
 
 from store.models import Book
+from store.permissions import IsOwnerOrStaffOrReadOnly
 from store.serializers import BookSerializer
 
 
@@ -12,10 +13,15 @@ class BookViewSet(ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [IsOwnerOrStaffOrReadOnly]
     filterset_fields = ['price']
     search_fields = ['name', 'author_name']
     ordering_fields = ['price', 'author_name']
+
+    def perform_create(self, serializer):
+        #присвоить пользователя книге который ее создал
+        serializer.validated_data['owner'] = self.request.user
+        serializer.save()
 
 def auth(request):
     return render(request, 'oauth.html')
